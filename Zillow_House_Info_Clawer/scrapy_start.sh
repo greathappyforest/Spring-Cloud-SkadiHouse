41af5e168b64f77d0b24ad711e25f7707c03c208
greathappyforest <122391887@qq.com>
2018-01-22 03:25:25 -0800

    Add files via upload

--- Zillow_House_Info_Clawer/scrapy_start.sh
+++ Zillow_House_Info_Clawer/scrapy_start.sh
@@ -1,8 +1,1 @@
-#! /bin/sh   
-export PATH=$PATH:/usr/local/bin  
-  
-#进入.py脚本所在目录  
-cd /Spring-Cloud-SkadiHouse/Zillow_House_Info_Clawer 
-  
-#执行.py中定义的项目example，并指定日志文件，其中nohup....&表示可以在后台执行，不会因为关闭终端而导致程序执行中断。  
-nohup scrapy crawl skadihouse &
+cd /Spring-Cloud-SkadiHouse/Zillow_House_Info_Clawer && /usr/bin/python get_proxy_list.py && sleep 3 && export PATH=$PATH:/usr/local/bin && scrapy crawl skadihouse >> /Spring-Cloud-SkadiHouse/Zillow_House_Info_Clawer/log/`date +\%Y-\%m-\%d_\%H:\%M:\%S-cron.log` 2>&1 &
